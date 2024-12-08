cbe6b1b591a698600745c6ac8e76fffb78cf3e9f
felipeSilv4Dev <felipesilva.developer@gmail.com>
2024-12-08 19:15:02 -0300

    fixing config

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,20 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      'next/core-web-vitals',
+      'next/typescript',
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+    ],
     files: ['**/*.{ts,tsx}'],
+
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -19,10 +25,12 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      '@typescript-eslint/no-empty-interface': 'off',
+
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
     },
-  },
-)
+  }
+);
